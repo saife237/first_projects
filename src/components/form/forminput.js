d0339407d243bf4148309efe4ab66ef6baf64d31
saife237 <saifedev@gmail.com>
2022-04-06 17:56:29 +0530

    New Update

--- src/components/form/forminput.js
+++ src/components/form/forminput.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
  class forminput extends Component {
 
 
@@ -7,37 +8,66 @@ import React, { Component } from 'react'
       super(props)
     
       this.state = {
-         userName:'',
+        fname:'',
+        lname:'',
          email:'',
+         date:'',
       }
     }
     handleUserNameChange = (event) => {
 this.setState({
-    userName:event.target.value
+  fname:event.target.value
 })
     }
+    handleLnameChange = (event) => {
+      this.setState({
+          lname:event.target.value
+      })
+  }
+
     handleEmailChange = (event) => {
         this.setState({
             email:event.target.value
         })
     }
-    submitChangeHandler = (event) => {
-event.preventDefault()
+
+    handleDateChange = (event) => {
+      this.setState ({
+        date:event.target.value
+      })
     }
+
+    submitChangeHandler = (event) => {
+      event.preventDefault();
+      }
   render() {
-      const {userName,email}=this.state
+      const {fname,email,lname,date,Message}=this.state
     return (
+      <div className='conatiner'>
         <form onSubmit={this.submitChangeHandler}>
       <div>
-              <label>UserName</label>
-              <input type="text" value={userName} onChange={this.handleUserNameChange}/>
+              <label className='fname'>FirstName</label>
+              <input className='input' type="text" value={fname} onChange={this.handleUserNameChange}/>
       </div>
       <div>
-      <label>Email</label>
-              <input type="email" value={email} onChange={this.handleEmailChange}/>
+        <label className='lname'>LastName</label>
+        <input className='input' type='text' value={lname} onChange={this.handleLnameChange}/>
+      </div>
+      <div>
+        <label className='email'>Email</label>
+        <input className='input' type='email' value={email} onChange={this.handleEmailChange}/>
+      </div>
+      <div>
+        <label className='date'>Date Of Birth</label>
+        <input className='input' type='date' value={date} onChange={this.handleDateChange}/>
+      </div>
+      <div>
+        <label className='Message'>Message</label>
+        <textarea className='input'/>
       </div>
       <button type='submit'>Submit</button>
       </form>
+      </div>
     )
   }
 }
